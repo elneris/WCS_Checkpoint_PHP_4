92176b71d08930986675f5c150dd056f9fc7ee3b
Elneris Dang <elneris.dang@gmail.com>
2019-07-17 15:28:07 +0200

    ajout d'umpload des image

---
+++ src/Form/PictureType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Job;
+use App\Entity\Picture;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PictureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', FileType::class, [
+                'label' => ' ',
+                'data_class' => null
+            ])
+            ->add('job', EntityType::class, [
+                'class' => Job::class,
+                'choice_label' => 'name',
+                'required' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Picture::class,
+        ]);
+    }
+}
